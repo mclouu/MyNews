cdcfcfe4657b58e9c81d5b569d178e1715044824
romain <rmathieu13@gmail.com>
2018-04-26 00:02:49 +0200

    remove Retrofit and implement rxjava

--- app/src/main/java/com/romain/mathieu/mynews/Controller/Fragment/TopStoriesPageFragment.java
+++ app/src/main/java/com/romain/mathieu/mynews/Controller/Fragment/TopStoriesPageFragment.java
@@ -7,36 +7,29 @@ package com.romain.mathieu.mynews.Controller.Fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
-import com.romain.mathieu.mynews.Model.API.MostPopular.NYTAPI;
 import com.romain.mathieu.mynews.Model.CardData;
-import com.romain.mathieu.mynews.Model.NYTService;
 import com.romain.mathieu.mynews.R;
 import com.romain.mathieu.mynews.View.MyAdapter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 public class TopStoriesPageFragment extends Fragment {
 
-    public static List<NYTAPI> responseBody;
     @BindView(R.id.top_stories_recyclerView)
     RecyclerView recyclerView;
     @BindView(R.id.top_stories_progressBar)
@@ -44,7 +37,7 @@ public class TopStoriesPageFragment extends Fragment {
     Context context;
     private LinearLayoutManager llm;
     private MyAdapter adapter;
-
+    private Disposable disposable;
 
     public TopStoriesPageFragment() {
         // Required empty public constructor
@@ -53,7 +46,6 @@ public class TopStoriesPageFragment extends Fragment {
     public static TopStoriesPageFragment newInstance() {
         return (new TopStoriesPageFragment());
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -71,56 +63,64 @@ public class TopStoriesPageFragment extends Fragment {
 
         ArrayList<CardData> list = new ArrayList<>();
 
-        getRetrofit();
-
         adapter = new MyAdapter(list);
         recyclerView.setAdapter(adapter);
 
 
-        // Inflate the layout for this fragment
+        streamShowString();
 
         return view;
 
 
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    //-----------------------
+    // RX JAVA
+    //-----------------------
+
+    // 1 - Create Observable
+    private Observable<String> getObservable() {
+        return Observable.just("Cool !");
+    }
+
+    // 2 - Create Subscriber
+    private DisposableObserver<String> getSubcriber() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    // 3 - Create Stream and execute it
+    private void streamShowString() {
+        this.disposable = this.getObservable()
+                .subscribeWith(getSubcriber());
+    }
+
+    // 5 - Dispose subscription
+    private void disposeWhenDestroy() {
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
+    }
 
 //    -----------------------
 //     HTTP REQUEST RETROFIT
 //    -----------------------
-
-    public void getRetrofit() {
-
-
-        RetrofitRequest.retrofitRequest();
-
-        NYTService service = RetrofitRequest.retrofit.create(NYTService.class);
-        //Call<List<NYTAPI>> call = service.getPostInfo(MyConstant.GET_SECTION(5));
-        //Call<List<NYTAPI>> call = service.getPostInfo("world");
-        Call<List<NYTAPI>> call = service.getPostInfo();
-
-
-        call.enqueue(new Callback<List<NYTAPI>>() {
-            @Override
-            public void onResponse(Call<List<NYTAPI>> call, Response<List<NYTAPI>> response) {
-                responseBody = response.body();
-                progressBar.setVisibility(View.GONE);
-
-                Log.e("RETROFIT", "OK " + responseBody);
-
-//                for (int i = 0; i < mListPost.size(); i++) {
-//                    list.add(new CardData(mListPost.get(i).getResults().get(i).getTitle(), mListPost.get(i).getResults().get(i).getAbstract()));
-//                }
-
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<List<NYTAPI>> call, @NonNull Throwable t) {
-                Toast.makeText(context, R.string.error_network, Toast.LENGTH_LONG).show();
-                Log.e("RETROFIT", "Network error");
-            }
-        });
-
-    }
 }
