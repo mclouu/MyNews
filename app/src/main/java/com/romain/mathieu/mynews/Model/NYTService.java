04a943e14e0b772e6e9e7a8dfa1658b3e4699c62
romain <rmathieu13@gmail.com>
2018-04-26 00:02:11 +0200

    update end point

--- app/src/main/java/com/romain/mathieu/mynews/Model/NYTService.java
+++ app/src/main/java/com/romain/mathieu/mynews/Model/NYTService.java
@@ -5,11 +5,16 @@
 package com.romain.mathieu.mynews.Model;
 
 import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.romain.mathieu.mynews.Model.API.NYTAPI;
+
+import java.util.List;
 
 import okhttp3.OkHttpClient;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface NYTService {
 
@@ -24,6 +29,6 @@ public interface NYTService {
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
-    @GET("svc/topstories/v2/home.json?api-key=e5ace90626ec4c7495500a0dbb327980")
-    Call<List<NYTAPI>> getPostInfo();
+    @GET("svc/topstories/v2/{section}.json?api-key=e5ace90626ec4c7495500a0dbb327980")
+    Call<List<NYTAPI>> getPostInfo(@Path("section") String section);
 }
