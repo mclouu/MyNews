fa5d5c9e596e873cd87af2e173a75008fab16a25
romain <rmathieu13@gmail.com>
2019-03-24 13:52:53 +0100

    clear test

--- app/src/test/java/com/romain/mathieu/mynews/reformatDateTest.java
+++ app/src/test/java/com/romain/mathieu/mynews/reformatDateTest.java
@@ -9,24 +9,17 @@ import static org.junit.Assert.assertEquals;
 
 public class reformatDateTest {
 
-    private ReformatDate reformatDate;
-
-
-    @Before
-    public void setUp() {
-        reformatDate = new ReformatDate();
-    }
 
     @Test
     public void returnBetterDateTopTest() {
 
-        assertEquals("2019-02-26 - 20:50:45", reformatDate.returnBetterDateTop("2019-02-26T20:50:45-05:00"));
+        assertEquals("2019-02-26 - 20:50:45", ReformatDate.returnBetterDateTop("2019-02-26T20:50:45-05:00"));
     }
 
 
     @Test
     public void returnBetterDateSearchTest() {
 
-        assertEquals("2019-02-27 - 17:21:57", reformatDate.returnBetterDateSearch("2019-02-27T17:21:57+0000"));
+        assertEquals("2019-02-27 - 17:21:57", ReformatDate.returnBetterDateSearch("2019-02-27T17:21:57+0000"));
     }
 }
